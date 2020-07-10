bd56907408ea670036eb000409147f3add336cd4
tdefriess <tyler.defriess@gmail.com>
2020-07-10 09:32:01 -0400

    Expensive sequence solved

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,4 +1,6 @@
 # Your code here
+import math
+import random
 
 
 def slowfun_too_slow(x, y):
@@ -15,6 +17,8 @@ def slowfun(x, y):
     output, but completes quickly instead of taking ages to run.
     """
     # Your code here
+    fact_table = {}
+    
 
 
 
