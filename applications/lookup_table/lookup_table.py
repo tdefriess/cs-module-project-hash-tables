2970b548d9869ee0b3bb1d2f21d3fe1b682e60aa
tdefriess <tyler.defriess@gmail.com>
2020-07-10 10:07:06 -0400

    Attempted lookup_table

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -11,15 +11,27 @@ def slowfun_too_slow(x, y):
 
     return v
 
-def slowfun(x, y):
+def slowfun(x, y, table):
     """
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
     # Your code here
-    fact_table = {}
+    v = math.pow(x, y)
+    v = table[v]
+    v //= (x + y)
+    v %= 982451653
+    return v
     
+def factorial(n, cache={}):
+    if n == 2:
+        return 2
+    else:
+        cache[n] = n * factorial(n, cache)
 
+    return cache
+
+fact_table = factorial(14 ** 6)
 
 
 # Do not modify below this line!
@@ -27,4 +39,4 @@ def slowfun(x, y):
 for i in range(50000):
     x = random.randrange(2, 14)
     y = random.randrange(3, 6)
-    print(f'{i}: {x},{y}: {slowfun(x, y)}')
+    print(f'{i}: {x},{y}: {slowfun(x, y, fact_table)}')
